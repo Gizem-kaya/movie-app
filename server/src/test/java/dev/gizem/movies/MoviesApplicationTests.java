cf8e302781c2516902a5d29f0f52855bc2da4e80
Gizem Kaya <violivaldi@gmail.com>
2024-01-10 12:27:50 +0100

    initial commit

---
+++ server/src/test/java/dev/gizem/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gizem.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
