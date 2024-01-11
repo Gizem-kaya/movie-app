749880a61dac142a714aa42f8451d73561f6e68c
Gizem Kaya <violivaldi@gmail.com>
2024-01-11 15:44:17 +0100

    ✨ movie query has been done

--- server/src/main/java/dev/gizem/movies/MovieController.java
+++ server/src/main/java/dev/gizem/movies/MovieController.java
@@ -2,17 +2,31 @@ package dev.gizem.movies;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> allMovies() {
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> allMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.getSingleMovie(imdbId), HttpStatus.OK);
     }
 
 }
