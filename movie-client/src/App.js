29e613056f104f5d657d6871ac2caca175936283
Gizem Kaya <violivaldi@gmail.com>
2024-01-12 14:39:54 +0100

    ✨ browser router added

--- movie-client/src/App.js
+++ movie-client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -21,17 +24,13 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Movies</h1>
+    <div className='App'>
       {movies ? (
-        <ul>
-          {movies.map((movie, index) => (
-            <li key={index}>{movie.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading movies...</p>
-      )}
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home movies={movies} />} />
+          </Route>
+        </Routes>) : (<h1>Loading...</h1>)}
     </div>
   );
 };
